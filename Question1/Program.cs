26e124e4d6c29a0ed85fbd9f17d1d72e019aad52
Ahmet Vepayev <ahmetwepaogly@gmail.com>
2022-04-27 01:05:50 +0300

    created project files

---
+++ Question1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Question2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
